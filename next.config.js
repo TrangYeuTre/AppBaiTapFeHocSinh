d2f5c6639837086b569b0255087b67e7ac414a84
trannghia91vn <trannghia213@gmail.com>
2024-04-17 19:08:21 +0700

    Submit cái cho chắc để clone về ec2

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const FB_API_KEY = "AIzaSyB98l0paywTCBKfAWxkJFnLpVpUG_e13D4";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    API_HOCSINH:
-      "https://ec2-54-251-92-120.ap-southeast-1.compute.amazonaws.com/api/v1",
-  },
   images: {
     remotePatterns: [
       {
