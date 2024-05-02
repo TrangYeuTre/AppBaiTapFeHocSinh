9102042acdf200468f9e2a80f37fdcc0b8c9c785
trannghia91vn <trannghia213@gmail.com>
2024-05-02 14:23:37 +0700

    Phải có nextconfig mới load ảnh được

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
